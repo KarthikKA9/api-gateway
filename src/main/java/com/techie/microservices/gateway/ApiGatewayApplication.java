541cf57ae231658646c225102d9a7f1fdf8fc52b
KarthikKA9 <81266223+KarthikKA9@users.noreply.github.com>
2024-10-27 13:19:42 -0500

    Updated Changes

--- src/main/java/com/techie/microservices/gateway/ApiGatewayApplication.java
+++ src/main/java/com/techie/microservices/gateway/ApiGatewayApplication.java
@@ -9,6 +9,8 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+        System.out.println("Hello Karthik");
     }
+
 
 }
